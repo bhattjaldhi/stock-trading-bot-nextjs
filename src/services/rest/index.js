b7f6b179b6224640c54951356c8ffe2ad05ef34f
Jaldhi Bhatt <jaldhibhatt@Jaldhis-Laptop.local>
2024-03-27 10:00:17 -0400

    Fix

--- src/services/rest/index.js
+++ src/services/rest/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
+import https from 'https'
 
 const instance = axios.create({
     baseURL: 'http://3.128.172.31',
-    headers: { Authorization: 'Bearer eyJhbGciOiJIUd2VyIn0.NDFAApMqRBwacpLumnyC_p7IWWmmWEFmXJIVkRoIA-I' }
+    headers: { Authorization: 'Bearer eyJhbGciOiJIUd2VyIn0.NDFAApMqRBwacpLumnyC_p7IWWmmWEFmXJIVkRoIA-I' },
+    httpsAgent: new https.Agent({ rejectUnauthorized: false }) 
 })
 
 export default {
