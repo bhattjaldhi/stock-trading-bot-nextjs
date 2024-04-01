9cbaf4d8d775974e9c3e23988b6ebffd613fe3a4
Fabio D. Junior <fabiojr@skiff.com>
2024-04-01 10:12:54 -0400

    Update index.js

--- src/services/rest/index.js
+++ src/services/rest/index.js
@@ -5,7 +5,8 @@ const instance = axios.create({
    //  baseURL: 'http://3.128.172.31',
     baseURL: 'https://06af-99-228-163-90.ngrok-free.app',
     headers: { Authorization: 'Bearer eyJhbGciOiJIUd2VyIn0.NDFAApMqRBwacpLumnyC_p7IWWmmWEFmXJIVkRoIA-I',
-              'ngrok-skip-browser-warning': 'true'},
+              'ngrok-skip-browser-warning': 'true',
+              'User-Agent': 'Airbot'},
     httpsAgent: new https.Agent({ rejectUnauthorized: false }) 
 })
 
