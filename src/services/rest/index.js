322343bfa37756760f9b5c138ab185947a76fcc1
Fabio D. Junior <fabiojr@skiff.com>
2024-04-01 17:25:12 -0400

    Update index.js

--- src/services/rest/index.js
+++ src/services/rest/index.js
@@ -2,15 +2,18 @@ import axios from 'axios'
 import https from 'https'
 
 const instance = axios.create({
-   //  baseURL: 'http://3.128.172.31',
-    baseURL: 'https://06af-99-228-163-90.ngrok-free.app',
+   //  baseURL: 'http://3.128.172.31', direct AWS IP
+    baseURL: 'https://piglet-moved-yearly.ngrok-free.app', // NGROK free domain with https
     headers: { Authorization: 'Bearer eyJhbGciOiJIUd2VyIn0.NDFAApMqRBwacpLumnyC_p7IWWmmWEFmXJIVkRoIA-I',
-              'ngrok-skip-browser-warning': 'true',
-              'User-Agent': 'Airbot'},
+              'ngrok-skip-browser-warning': 'true'
+             },
     httpsAgent: new https.Agent({ rejectUnauthorized: false }) 
 })
 
 export default {
     chat: (params) => instance.post('/chatbot/ask', params),
-    simulate: (params) => instance.get('/bot/simulate', { params }),
+    // @To Do: remove this comments in case of success =]   
+    // simulate: (params) => instance.get('/bot/simulate', { params }),
+    // Fabio: trying to bypass ngrok's browser validation by using post instead of get
+    simulate: (data) => instance.post('/bot/simulate', data),
 }
