0856861caedb3006631ebde2d67cf6588df032d8
Fabio D. Junior <fabiojr@skiff.com>
2024-04-01 09:53:19 -0400

    Update index.js

--- src/services/rest/index.js
+++ src/services/rest/index.js
@@ -3,7 +3,7 @@ import https from 'https'
 
 const instance = axios.create({
    //  baseURL: 'http://3.128.172.31',
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://06af-99-228-163-90.ngrok-free.app',
     headers: { Authorization: 'Bearer eyJhbGciOiJIUd2VyIn0.NDFAApMqRBwacpLumnyC_p7IWWmmWEFmXJIVkRoIA-I' },
     httpsAgent: new https.Agent({ rejectUnauthorized: false }) 
 })
