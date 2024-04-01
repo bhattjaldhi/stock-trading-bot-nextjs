43145b90ec263b9c50bee287b203794417761d86
Fabio D. Junior <fabiojr@skiff.com>
2024-04-01 10:07:28 -0400

    Update index.js

--- src/services/rest/index.js
+++ src/services/rest/index.js
@@ -4,7 +4,8 @@ import https from 'https'
 const instance = axios.create({
    //  baseURL: 'http://3.128.172.31',
     baseURL: 'https://06af-99-228-163-90.ngrok-free.app',
-    headers: { Authorization: 'Bearer eyJhbGciOiJIUd2VyIn0.NDFAApMqRBwacpLumnyC_p7IWWmmWEFmXJIVkRoIA-I' },
+    headers: { Authorization: 'Bearer eyJhbGciOiJIUd2VyIn0.NDFAApMqRBwacpLumnyC_p7IWWmmWEFmXJIVkRoIA-I',
+              'ngrok-skip-browser-warning': 'true'},
     httpsAgent: new https.Agent({ rejectUnauthorized: false }) 
 })
 
